9fbe3ba000b1a511c8e1657614b9096f03f5f90e
Prashannaa Rajan <136809249+PrashannaaRajan@users.noreply.github.com>
2025-07-07 20:42:59 -0400

    remove frontend hosting

--- backend/src/api/db.ts
+++ backend/src/api/db.ts
@@ -3,10 +3,11 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false,
-    },
- });
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 export default pool;
