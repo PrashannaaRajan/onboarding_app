a66b404ee4748dff787835ec8025e87bc883ffe8
Prashannaa Rajan <136809249+PrashannaaRajan@users.noreply.github.com>
2025-07-11 11:48:22 -0400

    fix ssl issue for sequelize

--- backend/src/api/db.ts
+++ backend/src/api/db.ts
@@ -3,4 +3,10 @@ import { Sequelize } from "sequelize";
 export const sequelize = new Sequelize(process.env["DATABASE_URL"] || "", {
   dialect: "postgres",
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Set to true if you have a valid CA cert
+    },
+  },
 });
